636bcca26762dc8786b3db367a1d43ae72cdb328
smilefan1993 <smile-fan@yandex.com>
2017-03-25 02:04:15 +0300

    set up framework

--- app/Config/core.php
+++ app/Config/core.php
@@ -34,7 +34,7 @@
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-	Configure::write('debug', 2);
+	Configure::write('debug', 1);
 
 /**
  * Configure the Error handler used to handle errors for your application. By default
@@ -225,12 +225,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'zat0Cd6z56tIrrUH265v1uglB5geQuo5zED1RJ97ESsauyBBrIk6MtfCTy4T2V9W');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '7403069993574508762683384576047047826428');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
